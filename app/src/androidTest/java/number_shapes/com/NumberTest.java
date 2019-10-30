d15ad79cf3cda72b89b088bbd400bb336de6688e
Marcin <marcin96semkowski@gmail.com>
2019-10-30 18:23:05 +0100

    Adding isSquareTest

--- app/src/androidTest/java/number_shapes/com/NumberTest.java
+++ app/src/androidTest/java/number_shapes/com/NumberTest.java
@@ -1,10 +1,13 @@
 package number_shapes.com;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class NumberTest {
+
+
 
     @Test
     public void isTriangularTest(){
@@ -16,5 +19,18 @@ public class NumberTest {
         //then
         assertTrue(isTriangular);
     }
+
+    @Test
+    public void isSquareTest(){
+        //given
+        int userNumberTest = 9;
+        Number  number = new Number(userNumberTest);
+         //when
+        boolean isSquare = number.isSquare();
+        //then
+        assertTrue(isSquare);
+    }
+
+
 
 }
