b0759496085af0e1a3329907529e953bacdb094c
Marcin <marcin96semkowski@gmail.com>
2019-10-30 19:26:31 +0100

    Adding update

--- app/src/main/java/number_shapes/com/MainActivity.java
+++ app/src/main/java/number_shapes/com/MainActivity.java
@@ -3,8 +3,44 @@ package number_shapes.com;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    public void testNumber(View view) {
+        EditText userValue = findViewById(R.id.Number);
+
+        Number number = new Number(Integer.parseInt(userValue.getText().toString()));
+
+        String message = "";
+
+        if ((userValue.getText().toString().isEmpty())) {
+            message = "please enter a number !";
+        } else {
+
+
+            if (number.isSquare()) {
+                if (number.isTriangular()) {
+                    message = number.getNumber() + "is both triangular and square !";
+
+                } else {
+                    message = number.getNumber() + "is   square but not triangular !";
+                }
+            } else {
+                if (number.isTriangular()) {
+                    message = number.getNumber() + "is  triangular but not square !";
+
+                } else {
+                    message = number.getNumber() + " is nether square or triangular !";
+                }
+            }
+            Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
+        }
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
